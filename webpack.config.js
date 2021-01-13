2d5605dd199b18ce100515bd1b8be57230b7e24b
Taly <vitalik7tv@yandex.ru>
2021-01-13 23:01:48 +0300

    remove needless slash

--- webpack.config.js
+++ webpack.config.js
@@ -16,7 +16,7 @@ module.exports = {
           {
             loader: MiniCssExtractPlugin.loader,
             options: {
-              publicPath: '/',
+              publicPath: '',
             },
           },
           'css-loader',
